8a10eca2ed0cbf0b14e34227455557bc7926236d
trinath00a <123577880+trinath00a@users.noreply.github.com>
2023-01-26 12:32:06 +0530

    Update TestStringFormatter.java

--- TestStringFormatter.java
+++ TestStringFormatter.java
@@ -1,6 +1,6 @@
 public class TestStringFormatter {  
 public static void main(String[] args) {  
-    System.out.println(StringFormatter.reverseString("my name is khan"));  
-    System.out.println(StringFormatter.reverseString("I am sonoo jaiswal"));      
+    System.out.println(StringFormatter.reverseString("my name is trinath"));  
+    System.out.println(StringFormatter.reverseString("I am from hyderabad"));      
     }  
 }  
