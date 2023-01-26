ba490cdc8fadc3ed097dc5ffe67c58ce9d377922
trinath <trinathvennapusa123@gmail.com>
2023-01-26 16:04:38 +0530

    Rev method

---
+++ Project1/src/rev.java
@@ -0,0 +1,18 @@
+
+public class rev {
+
+	public static void main(String[] args) {
+		String str="Hi trinath";
+	char []ch=str.toCharArray();
+	String rev1=" ";
+	for(int i=ch.length-1;i>=0;i--) {
+		//System.out.println(ch[i]);
+		rev1+=ch[i];
+	}
+	System.out.println(rev1);
+		
+	
+
+	}
+
+}
